91727dc5d1748e16b7ee9003e71c813ae3ba6b16
qiang,miao <miaoqiang@miao>
2016-12-19 22:04:30 +0800

    test commint

--- src/test/test.java
+++ src/test/test.java
@@ -3,7 +3,8 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
- System.out.println("my firest git app");
+	 System.out.println("my firest git app");
+	 System.out.println("my firest git app");
 	}
 
 }
